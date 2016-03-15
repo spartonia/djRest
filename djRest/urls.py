d52d5388cff45deffebe0f988ab88a783cd98cfa
spartonia <sd_prtn@yahoo.com>
2016-03-15 17:20:31 +0100

    func based views done(tutorial 2)

--- djRest/urls.py
+++ djRest/urls.py
@@ -9,6 +9,6 @@ router.register('groups', views.GroupViewSet)
 
 urlpatterns = [
     url(r'^', include('snippets.urls')),
-    url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    # url(r'^', include(router.urls)),
+    # url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
