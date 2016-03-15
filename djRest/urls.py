875f1911a7b73f52fd87603636745ab2e934d389
spartonia <sd_prtn@yahoo.com>
2016-03-15 16:58:44 +0100

    snippet serializer done

--- djRest/urls.py
+++ djRest/urls.py
@@ -8,6 +8,7 @@ router.register(r'users', views.UserViewSet)
 router.register('groups', views.GroupViewSet)
 
 urlpatterns = [
+    url(r'^', include('snippets.urls')),
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
